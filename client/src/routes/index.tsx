982e558811cc1a3485335e7f7d04c7454f131b54
Horob1 <nguyentheanh24102003@gmail.com>
2025-03-03 18:05:50 +0700

    init

---
+++ client/src/routes/index.tsx
@@ -0,0 +1,15 @@
+// const router = createBrowserRouter([
+//   {
+//     path: "/",
+//     element: <Layout />, // Layout bao quanh các trang
+//     children: [
+//       { index: true, element: <Home /> },
+//       { path: "about", element: <About /> },
+//       { path: "*", element: <NotFound /> }, // Route 404
+//     ],
+//   },
+// ]);
+
+// export default function AppRouter() {
+//   return <RouterProvider router={router} />;
+// }
